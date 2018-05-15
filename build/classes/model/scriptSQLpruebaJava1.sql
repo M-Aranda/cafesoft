4fb7e2973be82a998ac78b239ac78abe6d13c6d1
Eduardo Adasme <eadasme@svr.lab315>
2018-05-15 09:06:05 -0400

    Script sql UP

--- build/classes/model/scriptSQLpruebaJava1.sql
+++ build/classes/model/scriptSQLpruebaJava1.sql
@@ -1,3 +1,66 @@
-CREATE DATABASE prueba_java2;
+CREATE DATABASE cafesoft;
 
-USE prueba_java2;
+USE cafesoft;
+
+
+CREATE TABLE tipo_vivienda(
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE tipo_usuario(
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(20),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE cliente(
+    run VARCHAR(15),
+    nombre VARCHAR(50),
+    sueldo INT,
+    PRIMARY KEY(run)
+);
+
+CREATE TABLE vivienda(
+    n_rol INT AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    tipo_fk INT,
+    precio_arriendo INT,
+    precio_venta INT,
+    cant_baños INT,
+    cant_piezas INT,
+    direccion VARCHAR(50),
+    condicion VARCHAR(50),
+    PRIMARY KEY(n_rol),
+    FOREIGN KEY(tipo_fk) REFERENCES tipo_vivienda(id)
+);
+
+CREATE TABLE usuario(
+    run VARCHAR(15),
+    nombre VARCHAR(50),
+    tipo_fk INT,
+    PRIMARY KEY(run),
+    FOREIGN KEY(tipo_fk) REFERENCES tipo_usuario(id)
+);
+
+CREATE TABLE log(
+    id INT AUTO_INCREMENT,
+    descripcion VARCHAR(50),
+    fecha DATE,
+    usuario_fk VARCHAR(15),
+    PRIMARY KEY (id),
+    FOREIGN KEY(usuario_fk) REFERENCES usuario(run)
+);
+
+CREATE TABLE venta(
+    id INT AUTO_INCREMENT,
+    cliente_fk VARCHAR(15),
+    usuario_fk VARCHAR(15),
+    vivienda_fk INT,
+    fecha DATE,
+    PRIMARY KEY(id),
+    FOREIGN KEY(cliente_fk) REFERENCES cliente(run),
+    FOREIGN KEY(usuario_fk) REFERENCES usuario(run),
+    FOREIGN KEY(vivienda_fk) REFERENCES vivienda(n_rol)
+);
