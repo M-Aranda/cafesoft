aa0e0abf5d695324ea12a8027784534d5dac5138
Marcelo A <cheloz_20@hotmail.com>
2018-05-15 08:42:19 -0400

    creado login tentativo

--- src/org/adasme_aranda_barrera_gatica_vergara/prueba1_java_grupo5/gui/App.java
+++ src/org/adasme_aranda_barrera_gatica_vergara/prueba1_java_grupo5/gui/App.java
@@ -14,8 +14,23 @@ import javax.swing.UIManager;
  * Autores: Lalo Adasme Marcelo Aranda Franco Barrera Marcelo Gatica Javier
  * Vergara
  */
+
+
+
 public class App extends javax.swing.JFrame {
 
+    
+      //clase hilo creada dentro de app, para modificar componetes
+    private class HiloDePrueba extends Thread {
+
+        @Override
+        public void run() {
+            
+        }
+
+    }
+
+    
     //query para los inserts
     static final String WRITE_OBJECT_SQL = "INSERT INTO ejem(nombre, valor_objeto) VALUES (?, ?)";// modificar segun sea necesario
 
@@ -27,18 +42,12 @@ public class App extends javax.swing.JFrame {
      */
     public App() {
         initComponents();
+        this.setTitle("Inicio de sesión");
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
     }
 
-    //clase hilo creada dentro de app, para modificar componetes
-    private class HiloDePrueba extends Thread {
-
-        @Override
-        public void run() {
-            System.out.println("Aun no implementado");
-        }
-
-    }
-
+  
     //para escribir el objeto
     public static long writeJavaObject(Connection conn, Object object) throws Exception {
         String className = object.getClass().getName();
@@ -89,21 +98,73 @@ public class App extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jFrame1 = new javax.swing.JFrame();
+        jToggleButton1 = new javax.swing.JToggleButton();
+        txtRun = new javax.swing.JTextField();
+        lblRUN = new javax.swing.JLabel();
+        btnIniciarSesion = new javax.swing.JButton();
+
+        jToggleButton1.setText("jToggleButton1");
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
+        jFrame1.getContentPane().setLayout(jFrame1Layout);
+        jFrame1Layout.setHorizontalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jFrame1Layout.createSequentialGroup()
+                .addContainerGap(151, Short.MAX_VALUE)
+                .addComponent(jToggleButton1)
+                .addGap(144, 144, 144))
+        );
+        jFrame1Layout.setVerticalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jFrame1Layout.createSequentialGroup()
+                .addGap(90, 90, 90)
+                .addComponent(jToggleButton1)
+                .addContainerGap(187, Short.MAX_VALUE))
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        lblRUN.setText("R.U.N");
+
+        btnIniciarSesion.setText("Iniciar sesión");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(115, Short.MAX_VALUE)
+                .addComponent(lblRUN)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnIniciarSesion)
+                    .addComponent(txtRun, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(151, 151, 151))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(86, 86, 86)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtRun, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblRUN))
+                .addGap(18, 18, 18)
+                .addComponent(btnIniciarSesion)
+                .addContainerGap(153, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -148,5 +209,10 @@ public class App extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnIniciarSesion;
+    private javax.swing.JFrame jFrame1;
+    private javax.swing.JToggleButton jToggleButton1;
+    private javax.swing.JLabel lblRUN;
+    private javax.swing.JTextField txtRun;
     // End of variables declaration//GEN-END:variables
 }
