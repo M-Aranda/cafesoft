b173eaa67f8ff635e0d6e22163f95845e8445498
Eduardo Adasme <eadasme@svr.lab315>
2018-05-17 20:13:20 +0200

    Script actualizado

--- src/model/scriptSQLpruebaJava1.sql
+++ src/model/scriptSQLpruebaJava1.sql
@@ -22,18 +22,25 @@ CREATE TABLE cliente(
     PRIMARY KEY(run)
 );
 
+CREATE TABLE disponibilidad_vivienda(
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    PRIMARY KEY(id)
+);
+
 CREATE TABLE vivienda(
     n_rol INT AUTO_INCREMENT,
-    nombre VARCHAR(50),
     tipo_fk INT,
+    dis_vivienda INT,
     precio_arriendo INT,
     precio_venta INT,
     cant_baños INT,
     cant_piezas INT,
     direccion VARCHAR(50),
-    condicion VARCHAR(50),
+    condicion BOOLEAN,
     PRIMARY KEY(n_rol),
-    FOREIGN KEY(tipo_fk) REFERENCES tipo_vivienda(id)
+    FOREIGN KEY(tipo_fk) REFERENCES tipo_vivienda(id),
+    FOREIGN KEY(dis_vivienda) REFERENCES disponibilidad_vivienda(id)
 );
 
 CREATE TABLE usuario(
@@ -53,7 +60,7 @@ CREATE TABLE log(
     FOREIGN KEY(usuario_fk) REFERENCES usuario(run)
 );
 
-CREATE TABLE venta(
+CREATE TABLE contrato(
     id INT AUTO_INCREMENT,
     cliente_fk VARCHAR(15),
     usuario_fk VARCHAR(15),
@@ -64,3 +71,10 @@ CREATE TABLE venta(
     FOREIGN KEY(usuario_fk) REFERENCES usuario(run),
     FOREIGN KEY(vivienda_fk) REFERENCES vivienda(n_rol)
 );
+
+INSERT INTO tipo_vivienda VALUES (NULL,'Casa');
+INSERT INTO tipo_vivienda VALUES (NULL,'Departamento');
+
+INSERT INTO disponibilidad_vivienda VALUES (NULL, 'Arrendada');
+INSERT INTO disponibilidad_vivienda VALUES (NULL, 'Vendida');
+INSERT INTO disponibilidad_vivienda VALUES (NULL, 'Disponible');
